bdb0bc0500accef7444d5cda2c0d79c33e9da6ae
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-11-10 00:07:38 -0500

    ✍🏽 Initial commit

---
+++ learning.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CSharpTutorials
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      string message = "Hello World!!";
+
+      Console.WriteLine(message);
+    }
+  }
+}
